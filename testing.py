6db2b5dd08740ab7109912546e890f0ce7c1ea19
feanor16 <chrwils3@cisco.com>
2017-03-30 19:52:51 +0000

    added new tests for hello/universe fixed

--- testing.py
+++ testing.py
@@ -22,7 +22,7 @@ class FlaskTestCase(unittest.TestCase):
 
     def test_universe_correct_content(self):
         resp = self.app.get('/hello/universe')
-        self.assertEquals(resp.data, '"Hello Universe!"\n'
+        self.assertEquals(resp.data, '"Hello Universe!"\n')
 
     def tearDown(self):
         pass
